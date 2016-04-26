e1b3f49b30381caa3adad7819168be51202b0f5b
Ivan Bruel <ivan.bruel@gmail.com>
2016-04-26 10:59:31 +0100

    Created a CloseableViewController protocol to show a close button on the left of the navigation bar Also handle closing by itself

---
+++ Reddit/Reddit/Protocols/ViewControllers/CloseableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CloseableViewController.swift
+//  Reddit
+//
+//  Created by Ivan Bruel on 26/04/16.
+//  Copyright © 2016 Faber Ventures. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import NSObject_Rx
+
+protocol CloseableViewController {
+
+  func setupCloseButton()
+
+}
+
+extension CloseableViewController where Self: UIViewController {
+
+  func setupCloseButton() {
+    guard let firstViewController = navigationController?.viewControllers[0]
+      where firstViewController == self else {
+        return
+    }
+    
+    let closeButton = UIBarButtonItem(title: tr(.CloseableButtonClose), style: .Plain, target: nil,
+                                      action: nil)
+    closeButton.rx_tap
+      .bindNext { _ in
+        self.dismissViewControllerAnimated(true, completion: nil)
+      }.addDisposableTo(rx_disposeBag)
+    navigationItem.leftBarButtonItem = closeButton
+  }
+}
