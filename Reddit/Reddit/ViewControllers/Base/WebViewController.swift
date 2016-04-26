5a7b6dc95f3bbde3db8fe1cb9cd3a7d3e154955f
Ivan Bruel <ivan.bruel@gmail.com>
2016-04-26 11:00:12 +0100

    Base WebViewController for subclassing Has a ProgressView to show web view loading progress

---
+++ Reddit/Reddit/ViewControllers/Base/WebViewController.swift
@@ -0,0 +1,83 @@
+//
+//  WebViewController.swift
+//  Reddit
+//
+//  Created by Ivan Bruel on 26/04/16.
+//  Copyright © 2016 Faber Ventures. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+// MARK: Properties, Lifecycle and API
+class WebViewController: UIViewController {
+
+  let webView = WKWebView()
+  let progressView: UIProgressView = UIProgressView(progressViewStyle: .Bar)
+
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setupViews()
+  }
+
+  func loadURL(URLString: String) {
+    guard let url = NSURL(string: URLString) else {
+      print("\(URLString) is not a valid URL string")
+      return
+    }
+    webView.loadRequest(NSURLRequest(URL: url))
+  }
+}
+
+//MARK: Setup
+extension WebViewController: WKNavigationDelegate {
+
+  private func setupViews() {
+    setupWebView()
+    setupProgressView()
+  }
+
+  private func setupWebView() {
+    webView.navigationDelegate = self
+    view.addSubview(webView)
+    webView.snp_makeConstraints { (make) -> Void in
+      make.edges.equalTo(view)
+    }
+  }
+
+  private func setupProgressView() {
+    view.addSubview(progressView)
+
+    progressView.snp_makeConstraints { (make) in
+      make.top.equalTo(self.snp_topLayoutGuideBottom)
+      make.left.right.equalTo(view)
+    }
+
+    webView.rx_observe(Double.self, "estimatedProgress")
+      .bindNext { estimatedProgress in
+        guard let estimatedProgress = estimatedProgress else {
+          return
+        }
+        self.progressView.setProgress(Float(estimatedProgress), animated: true)
+        if estimatedProgress == 1 {
+          self.hideProgressView()
+        } else {
+          self.progressView.alpha = 1
+        }
+      }.addDisposableTo(rx_disposeBag)
+  }
+}
+
+// MARK: Animations
+extension WebViewController {
+
+  private func hideProgressView() {
+    UIView.animateWithDuration(0.3, delay: 0.5, options: [], animations: {
+      self.progressView.alpha = 0
+    }) { _ in
+      self.progressView.progress = 0
+    }
+  }
+}
