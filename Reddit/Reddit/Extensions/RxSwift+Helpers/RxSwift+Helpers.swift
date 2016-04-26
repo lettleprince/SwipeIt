327923907d55fa45718fb01b19c7b4e2158189ec
Ivan Bruel <ivan.bruel@gmail.com>
2016-04-26 10:55:31 +0100

    Added void global function for RxSwift map(void) calls in order to abstract content

---
+++ Reddit/Reddit/Extensions/RxSwift+Helpers/RxSwift+Helpers.swift
@@ -0,0 +1,15 @@
+//
+//  RxSwift+Helpers.swift
+//  Reddit
+//
+//  Created by Ivan Bruel on 26/04/16.
+//  Copyright © 2016 Faber Ventures. All rights reserved.
+//
+
+import Foundation
+
+func void<T>(_: T) -> Void {
+
+  return Void()
+  
+}
