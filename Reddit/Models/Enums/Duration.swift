95860f5105ccc3ec3e5734b5932a1b68dc62e18d
Ivan Bruel <ivan.bruel@gmail.com>
2016-05-03 18:28:05 +0100

    Added Scope and Duration enums for OAuth

---
+++ Reddit/Models/Enums/Duration.swift
@@ -0,0 +1,16 @@
+//
+//  Duration.swift
+//  Reddit
+//
+//  Created by Ivan Bruel on 03/05/16.
+//  Copyright © 2016 Faber Ventures. All rights reserved.
+//
+
+import Foundation
+
+enum Duration: String {
+
+  case Permanent = "permanent"
+  case Temporary = "temporary"
+
+}
---
+++ Reddit/Models/Enums/Scope.swift
@@ -0,0 +1,40 @@
+//
+//  Scope.swift
+//  Reddit
+//
+//  Created by Ivan Bruel on 03/05/16.
+//  Copyright © 2016 Faber Ventures. All rights reserved.
+//
+
+import Foundation
+
+enum Scope: String {
+
+  case Creddits = "creddits"
+  case ModContributors = "modcontributors"
+  case ModConfig = "modconfig"
+  case Subscribe = "subscribe"
+  case WikiRead = "wikiread"
+  case WikiEdit = "wikiedit"
+  case Vote = "vote"
+  case MySubreddits = "mysubreddits"
+  case Submit = "submit"
+  case ModLog = "modlog"
+  case ModPosts = "modposts"
+  case ModFlair = "modflair"
+  case Save = "save"
+  case ModOthers = "modothers"
+  case Read = "read"
+  case PrivateMessages = "privatemessages"
+  case Report = "report"
+  case Identity = "identity"
+  case LiveManage = "livemanage"
+  case Account = "account"
+  case ModTraffic = "modtraffic"
+  case Edit = "edit"
+  case ModWiki = "modwiki"
+  case ModSelf = "modself"
+  case History = "history"
+  case Flair = "flair"
+
+}
