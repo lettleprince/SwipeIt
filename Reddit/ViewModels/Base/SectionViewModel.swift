14d7f665ad7d63115e794ec5868a510e30fa0180
Ivan Bruel <ivan.bruel@gmail.com>
2016-05-09 12:18:06 +0100

    Created a generic SectionViewModel class for easier usage throughout the sectioned tableViews

---
+++ Reddit/ViewModels/Base/SectionViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  SubredditListSectionViewModel.swift
+//  Reddit
+//
+//  Created by Ivan Bruel on 04/05/16.
+//  Copyright © 2016 Faber Ventures. All rights reserved.
+//
+
+import Foundation
+import RxDataSources
+
+// Generic Section View Model for ViewModel implementations
+final class SectionViewModel<T>: NSObject, ViewModel {
+
+  let title: String
+  let viewModels: [T]
+
+  init(title: String, viewModels: [T]) {
+    self.title = title
+    self.viewModels = viewModels
+  }
+
+}
+
+extension SectionViewModel: SectionModelType {
+  typealias Item = T
+
+  var items: [Item] {
+    return viewModels
+  }
+
+  convenience init(original: SectionViewModel, items: [Item]) {
+    self.init(title: original.title, viewModels: items)
+  }
+}
