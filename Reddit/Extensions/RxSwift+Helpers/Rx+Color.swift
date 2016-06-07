1f7957df1d6e050afdfba4b22feb4f3691077f55
Ivan Bruel <ivan.bruel@gmail.com>
2016-06-07 18:13:16 +0100

    Added some Rx Color bindings to common color setters

---
+++ Reddit/Extensions/RxSwift+Helpers/Rx+Color.swift
@@ -0,0 +1,45 @@
+//
+//  UILabel+Rx.swift
+//  Reddit
+//
+//  Created by Ivan Bruel on 06/06/16.
+//  Copyright © 2016 Faber Ventures. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+// swiftlint:disable variable_name
+extension UILabel {
+
+  var rx_textColor: AnyObserver<UIColor> {
+    return UIBindingObserver(UIElement: self) { label, color in
+      label.textColor = color
+      }.asObserver()
+  }
+}
+
+extension UIButton {
+
+  var rx_titleColor: AnyObserver<UIColor> {
+    return UIBindingObserver(UIElement: self) { button, color in
+      button.setTitleColor(color, forState: .Normal)
+      }.asObserver()
+  }
+
+  var rx_color: AnyObserver<UIColor> {
+    return UIBindingObserver(UIElement: self) { button, color in
+      button.setTitleColor(color, forState: .Normal)
+      button.tintColor = color
+      }.asObserver()
+  }
+}
+
+extension UIView {
+  var rx_backgroundColor: AnyObserver<UIColor> {
+    return UIBindingObserver(UIElement: self) { view, color in
+      view.backgroundColor = color
+      }.asObserver()
+  }
+}
