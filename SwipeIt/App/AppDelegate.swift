7d6cc67981519b4f3c9139397fc8e243bb8c22f9
Ivan Bruel <ivan.bruel@gmail.com>
2016-09-06 17:22:11 +0100

    Clearing the disk cache upon app start

--- SwipeIt/App/AppDelegate.swift
+++ SwipeIt/App/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import Localizable
 import Fabric
 import Crashlytics
+import Kingfisher
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     Fabric.with([Crashlytics.self])
+    KingfisherManager.sharedManager.cache.clearDiskCache()
     //Localizable.setup()
     return true
   }
